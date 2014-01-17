dbba215b6f1a2c602786271d70049586c12be145
Arti Mathanda <artismarti@gmail.com>
2014-01-17 00:07:00 +0000

    Dry up.

--- directory.rb
+++ directory.rb
@@ -2,14 +2,25 @@ student_count = 7
 
 students = ["Olivia Dunham", "Buffy Summers", "Veronica Mars", "Arya Stark", "Cordelia Chase", "Anya Jenkins", "Arti Mathanda"]
 
-puts "The students of my cohort at Atlas"
-puts "-----------------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
+students.each do |students|
+	puts students
+end
 
-puts "Overall, we have #{students.length} kick-ass students!"
+def print_header
+	puts "The students of my cohort at Atlas:"
+	puts "-----------------------"
+end
+
+def print(names)
+	names.each do |name|
+		puts name
+	end
+end
+
+def print_footer(students)
+	puts "Overall, we have #{students.length} kick-ass students!"
+end
+
+print_header
+print(students)
+print_footer(students)
