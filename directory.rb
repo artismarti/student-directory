2bdbc4191c97de7dacdc1d2ec08031f684a49ea8
Arti Mathanda <artismarti@gmail.com>
2014-01-26 23:02:29 +0000

    User defined names & cohort

--- directory.rb
+++ directory.rb
@@ -1,14 +1,14 @@
 student_count = 7
 
-students = [
-	{:name => "Olivia Dunham", :cohort => :January},
-	{:name => "Buffy Summers", :cohort => :January},
-	{:name => "Veronica Mars", :cohort => :January},
-	{:name => "Arya Stark", :cohort => :January},
-	{:name => "Cordelia Chase", :cohort => :January},
-	{:name => "Anya Jenkins", :cohort => :January},
-	{:name => "Arti Mathanda", :cohort => :January},
-]
+#students = [
+#	{:name => "Olivia Dunham", :cohort => :January},
+#	{:name => "Buffy Summers", :cohort => :January},
+#	{:name => "Veronica Mars", :cohort => :January},
+#	{:name => "Arya Stark", :cohort => :January},
+#	{:name => "Cordelia Chase", :cohort => :January},
+#	{:name => "Anya Jenkins", :cohort => :January},
+#	{:name => "Arti Mathanda", :cohort => :January},
+#]
 
 def print_header
 	puts "The students of my cohort at Atlas:"
@@ -25,24 +25,28 @@ def print_footer(students)
 	puts "Overall, we have #{students.length} kick-ass students!"
 end
 
-#def input_students
-#	p "Please enter the names of the students:"
-#	puts "To finish, just hit return twice."
-#	#create students array
-#	students = []
-#	#get user input
-#	name = gets.chomp
-#	while !name.empty? do
-#		# add student hash to array
-#		students << {:name => name, :cohort => :January}
-#		puts "Now we have #{students.length} students."
-#		name = gets.chomp
-#	end
-#	#return student array
-#	students
-#end
+def input_students
+	p "Please enter the name of the students:"
+	puts "To finish, just hit return twice."
+	#get user input
+	name = gets.chomp.upcase
+	p "Please enter the cohort of the students:"
+	puts "To finish, just hit return twice."
+	cohort = gets.chomp.upcase
+	#create students array
+	students = []
+	while !name.empty? && !cohort.empty? do
+		# add student hash to array
+		students << {:name => name, :cohort => cohort}
+		puts "Now we have #{students.length} students."
+		name = gets.chomp.upcase
+		cohort = gets.chomp.upcase
+	end
+	#return student array
+	students
+end
 
-#students = input_students
+students = input_students
 print_header
 print(students)
 print_footer(students)
